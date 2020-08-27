124fef04fb9f7d961493e4f8fa5e76b6cdd9f942
Robert Kempton <r@snow87.com>
2020-08-26 22:01:25 -0600

    push down id generator to make it settable per save clean up comments and stuff

--- src/index.js
+++ src/index.js
@@ -4,13 +4,7 @@ const { promisify } = require( 'util' )
 const stringizer = require( './stringizer.js' )
 module.exports = {
     /**
-     * A createDb is a logical grouping of objects of similarish type.
-     *
-     * Collections should be as small as possible. For instance, a single user should have
-     * their own createDb of widgets instead of a single createDb of widgets that has every users data in it.
-     * Doing this will ensure that find operations and the like will remain speedy without the need for an index.
-     *
-     * Complex find operations that involve searching multiple collections or large collections can be achieved using the redisearch module
+     * Create a db object to use for saving arbitrary objects to redis.
      *
      * Since everything in redis is stored as a string, this library injects type hints by appending them to the keys with a delimiter.
      *
@@ -26,39 +20,39 @@ module.exports = {
 
      * @param serializers Specify custom jpflat serializers to use for serialization. The default is to use require('./stringizer.js')
      * @param deserializers Specify custom jpflat deserializers to use for deserialization. The default is to use require('./stringizer.js')
-     * @param pathReducer The path reducer to use to createDb the path. Default is json path reduce from jpflat
+     * @param pathReducer The path reducer to use to flatten objects. Default is json path reduce from jpflat
      * @param pathExpander The path expander to use to deserialize. Default is the stringizer pathExpander, which uses jsonpath and appends type information to the path
-     * @param idGenerator A function that receives the object being saved and generates a new id for it. The default is to createDb a bson objectid.
      * @returns {{save(Object), findOneById(*)}}
      */
     createDb( client,
-              serializers = [ stringizer ], deserializers = [ stringizer ],
-              pathReducer = stringizer.pathReducer, pathExpander = stringizer.pathExpander,
-              idGenerator = ()=>ObjectID().toString()
-    ) {
-        if( !client ) throw new Error( 'Client must be set before using a createDb' )
+              serializers = [ stringizer ],
+              deserializers = [ stringizer ],
+              pathReducer = stringizer.pathReducer,
+              pathExpander = stringizer.pathExpander ) {
+
+        if( !client ) throw new Error( 'Client must be set before using the db' )
         if( typeof client.send_command !== 'function' ) throw new Error( 'client must support send_command callback style function' )
         let cmd = promisify( client.send_command ).bind( client )
 
         return {
             /**
-             * Save an object and add it to the createDb
+             * Save an object and optionally add it to a collection
              *
              * @param obj The saved object
              * @param collectionKey An optional collection key
-             *
+             * @param idGenerator A function that receives the object being saved and generates a new id for it. The default is to create a bson objectid.
              */
-            async save( obj, collectionKey ) {
+            async save( obj, collectionKey, idGenerator = ( objToSave ) => ObjectID().toString() ) {
                 if( !obj || typeof obj !== 'object' )
                     throw new Error( 'You can only save truthy objects with redish' )
                 if( Array.isArray( obj ) && obj.length === 0 )
                     throw new Error( 'Empty arrays cannot be saved' )
                 let isNew = !obj.id
                 if( isNew ) {
-                    obj.id = idGenerator(obj)
+                    obj.id = idGenerator( obj )
                 }
                 let flatObj = await flatten( obj, serializers, pathReducer )
-                //begin transaction to ensure the createDb zset stays consistent
+                //begin transaction to ensure the zset stays consistent
                 await cmd( 'WATCH', [ obj.id ] )
                 let currentKeys = !isNew && await cmd( 'HKEYS', [ obj.id ] ) || []
                 await cmd( 'MULTI' )
@@ -69,7 +63,7 @@ module.exports = {
                     if( deletedKeys && deletedKeys.length > 0 )
                         await cmd( 'HDEL', [ obj.id, ...deletedKeys ] )
                 } else {
-                    if(collectionKey) await cmd( 'ZADD', [ collectionKey, obj.id, 0 ] )
+                    if( collectionKey ) await cmd( 'ZADD', [ collectionKey, obj.id, 0 ] )
                 }
                 //TODO add configurable retry
                 if( await cmd( 'EXEC' ) === null )
@@ -78,7 +72,7 @@ module.exports = {
                 return obj
             },
             /**
-             * Find one object in the createDb by it's id
+             * Find one object in the db by it's id
              *
              * @param id
              */
@@ -96,7 +90,7 @@ module.exports = {
 
             }
             // /**
-            //  * Find all of the objects stored in this createDb, one page at a time
+            //  * Find all of the objects stored in this collection, one page at a time
             //  * @param page The page number to get
             //  * @param size The number of objects to retrieve at a time
             //  */
@@ -104,13 +98,13 @@ module.exports = {
             //
             // },
             // /**
-            //  * Scan the createDb for a object that has the specified value for the field
+            //  * Scan the collection for a object that has the specified value for the field
             //  * TODO: support multiple fields and complex boolean statements (foo == 5 and (bar == yep or baz == nope))
             //  * @param field
             //  * @param value
             //  */
             // async findOneBy( collectionKey, field, value ) {
-            //     //for each document in each page of the createDb, scan the documents and search for matches
+            //     //for each document in each page of the collection, scan the documents and search for matches
             // }
         }
     }
