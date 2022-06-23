1413027198698c2e1b39fbdfd380fcca1b14316e
Robert Kempton <r@snow87.com>
2022-06-23 01:43:31 -0600

    allow passing an ajv validate function instead of compiling the schema for short lived collection objects

--- src/index.js
+++ src/index.js
@@ -7,25 +7,25 @@ const ajvKeywords = require('ajv-keywords')
 
 module.exports = {
   /**
-     * Create a db object to use for saving arbitrary objects to redis.
-     *
-     * Since everything in redis is stored as a string, this library injects type hints by appending them to the keys with a delimiter.
-     *
-     * This behavior can be overridden by specifying the pathReduce and pathExpander.
-     *
-     * You can also provide your own own serializer and deserializer to change the way values are converted to strings.
-     *
-     * TODO: Add basic single key indexing
-     * TODO: Add support for "sort indexes" for speedy paging using zsets on arbitrary keys
-     * TODO: Add support for unique constraints using sets
-     *
-     * @param client The redis client to use, this library assumes this implementation: https://www.npmjs.com/package/redis.
+   * Create a db object to use for saving arbitrary objects to redis.
+   *
+   * Since everything in redis is stored as a string, this library injects type hints by appending them to the keys with a delimiter.
+   *
+   * This behavior can be overridden by specifying the pathReduce and pathExpander.
+   *
+   * You can also provide your own own serializer and deserializer to change the way values are converted to strings.
+   *
+   * TODO: Add basic single key indexing
+   * TODO: Add support for "sort indexes" for speedy paging using zsets on arbitrary keys
+   * TODO: Add support for unique constraints using sets
+   *
+   * @param client The redis client to use, this library assumes this implementation: https://www.npmjs.com/package/redis.
 
-     * @param valueSerializers Specify custom jpflat valueSerializers to use for serialization. The default is to use require('./stringizer.js')
-     * @param valueDeserializers Specify custom jpflat valueDeserializers to use for deserialization. The default is to use require('./stringizer.js')
-     * @param pathReducer The path reducer to use to flatten objects. Default is json path reduce from jpflat
-     * @param pathExpander The path expander to use to deserialize. Default is the stringizer pathExpander, which uses jsonpath and appends type information to the path
-     */
+   * @param valueSerializers Specify custom jpflat valueSerializers to use for serialization. The default is to use require('./stringizer.js')
+   * @param valueDeserializers Specify custom jpflat valueDeserializers to use for deserialization. The default is to use require('./stringizer.js')
+   * @param pathReducer The path reducer to use to flatten objects. Default is json path reduce from jpflat
+   * @param pathExpander The path expander to use to deserialize. Default is the stringizer pathExpander, which uses jsonpath and appends type information to the path
+   */
   createDb (client, { valueSerializers, valueDeserializers, pathReducer, pathExpander } = {}) {
     if (!valueSerializers) valueSerializers = [stringizer]
     if (!Array.isArray(valueSerializers)) valueSerializers = [valueSerializers]
@@ -40,23 +40,36 @@ module.exports = {
 
     return {
       /**
-             * Create a collection of objects. The items in the collection are maintained in a zset to allow finding everything and pagination
-             * @param collectionKey The name of the collection. The collection key is appended to the idField to ensure unique keys across collections
-             * @param schema A json schema to use for validation on save
-             * @param idField The name of the idField to use as the unique Id. If unset, the 'id' field is used.
-             *                If set, it will be prefixed with the collection key to ensure unique keys across collections.
-             * @param ajv A pre-created instance of ajv to use for schema validation.
-             * @param ajvOptions An Options object to initialize ajv with. Not used if schema is not set or if ajv instance is passed.
-             *                   ajv-formats are installed when an ajv instance is not passed.
-             * @param ajvFormatsOptions An Options object to pass to ajv-formats. ajv-formats is not installed and these options are not used if an ajv instance is passed.
-             * @param ajvKeywordsOptions An Options object to pass to ajv-keywords. ajv-keywords is not installed and these options are not used if an ajv instance is passed.
-             * @param idGenerator A function that receives the object being saved and generates a new id for it. The default is to create a bson objectid.
-             * @param enableAudit Whether to enable auditing addition and management of auditing fields createdBy, createdAt, updatedBy, updatedAt
-             * @param {(object) => number} calculateScore A function to calculate the score to use when adding a new object to a zset.
-             *          This score determines the order records are returned in when calling findAll.
-             *          The default is to return the current millis at the time of storage
-             */
-      collection (collectionKey, { schema, idField, ajv, ajvOptions, ajvFormatsOptions, ajvKeywordsOptions, idGenerator, enableAudit, calculateScore } = {}) {
+       * Create a collection of objects. The items in the collection are maintained in a zset to allow finding everything and pagination
+       * @param collectionKey The name of the collection. The collection key is appended to the idField to ensure unique keys across collections
+       * @param validate A pre-compiled ajv function to use for validation.
+       *                Must follow the ajv model and set an errors property on the function after detecting an invalid object.
+       * @param schema A json schema to use for validation on save
+       * @param idField The name of the idField to use as the unique Id. If unset, the 'id' field is used.
+       *                If set, it will be prefixed with the collection key to ensure unique keys across collections.
+       * @param ajv A pre-created instance of ajv to use for schema validation.
+       * @param ajvOptions An Options object to initialize ajv with. Not used if schema is not set or if ajv instance is passed.
+       *                   ajv-formats are installed when an ajv instance is not passed.
+       * @param ajvFormatsOptions An Options object to pass to ajv-formats. ajv-formats is not installed and these options are not used if an ajv instance is passed.
+       * @param ajvKeywordsOptions An Options object to pass to ajv-keywords. ajv-keywords is not installed and these options are not used if an ajv instance is passed.
+       * @param idGenerator A function that receives the object being saved and generates a new id for it. The default is to create a bson objectid.
+       * @param enableAudit Whether to enable auditing addition and management of auditing fields createdBy, createdAt, updatedBy, updatedAt
+       * @param {(object) => number} calculateScore A function to calculate the score to use when adding a new object to a zset.
+       *          This score determines the order records are returned in when calling findAll.
+       *          The default is to return the current millis at the time of storage
+       */
+      collection (collectionKey, {
+        validate,
+        schema,
+        idField,
+        ajv,
+        ajvOptions,
+        ajvFormatsOptions,
+        ajvKeywordsOptions,
+        idGenerator,
+        enableAudit,
+        calculateScore
+      } = {}) {
         if (typeof collectionKey !== 'string' || collectionKey.length < 1) {
           throw new Error('collectionKey must be a non-empty string')
         }
@@ -72,8 +85,8 @@ module.exports = {
         if (!idField) {
           idField = 'id'
         }
-        let validate
-        if (schema) {
+
+        if (typeof validate !== 'function' && schema) {
           if (ajv) {
             validate = ajv.compile(schema)
           } else {
@@ -94,11 +107,12 @@ module.exports = {
           }
           return id.startsWith(keyPrefix) ? id : `${keyPrefix}${id}`
         }
+
         /**
-                 * Find one object in the db by it's id
-                 *
-                 * @param id The id to lookup
-                 */
+         * Find one object in the db by it's id
+         *
+         * @param id The id to lookup
+         */
         async function findOneById (id) {
           id = ensurePrefix(id)
           const res = await client.hGetAll(id)
@@ -118,7 +132,7 @@ module.exports = {
         async function doSave (obj, auditUser, isPatch) {
           if (!obj || typeof obj !== 'object') { throw new Error('You can only save truthy objects with redish') }
           if (Array.isArray(obj) && obj.length === 0) { throw new Error('Empty arrays cannot be saved') }
-          const isNew = !obj[idField] || (enableAudit && !obj.createdAt)
+          const isNew = !obj[idField] || ( enableAudit && !obj.createdAt )
           if (isNew) {
             if (!obj[idField]) obj[idField] = `${keyPrefix}${idGenerator(obj)}`
             if (enableAudit) {
@@ -151,9 +165,9 @@ module.exports = {
           await multi.hSet(obj[idField], Object.entries(flatObj))
           if (!isNew && !isPatch) {
             // delete any removed keys if this is not a patch
-            const currentKeys = !isNew && (await client.hKeys(obj[idField]) || [])
+            const currentKeys = !isNew && ( await client.hKeys(obj[idField]) || [] )
             // Get the current set of object keys and delete any keys that do not exist on the current object
-            const deletedKeys = currentKeys.filter((key) => !(key in flatObj))
+            const deletedKeys = currentKeys.filter((key) => !( key in flatObj ))
             if (deletedKeys && deletedKeys.length > 0) { await multi.hDel(obj[idField], ...deletedKeys) }
           } else if (isNew) {
             await multi.zAdd(collectionKey, { score: calculateScore(obj), value: obj[idField] })
@@ -161,37 +175,38 @@ module.exports = {
           await multi.exec()
           return obj
         }
+
         return {
           /**
-                     * Save an object and optionally add it to a collection
-                     *
-                     * This treats keys missing from the object as deleted keys.
-                     * To avoid this behavior, use upsert instead
-                     *
-                     * @param obj The object to save
-                     * @param auditUser The optional user identifier to use for the "By" audit fields
-                     */
+           * Save an object and optionally add it to a collection
+           *
+           * This treats keys missing from the object as deleted keys.
+           * To avoid this behavior, use upsert instead
+           *
+           * @param obj The object to save
+           * @param auditUser The optional user identifier to use for the "By" audit fields
+           */
           // TODO add list of keys to index
           async save (obj, auditUser) {
             return doSave(obj, auditUser, false)
           },
           /**
-                     * Upsert an object by id
-                     * This is similar to save except it only sets keys and will never delete existing keys
-                     *
-                     * If an id is not provided on the object one will be generated
-                     *
-                     * @param obj The object to save
-                     * @param auditUser The optional user identifier to use for the "By" audit fields
-                     */
+           * Upsert an object by id
+           * This is similar to save except it only sets keys and will never delete existing keys
+           *
+           * If an id is not provided on the object one will be generated
+           *
+           * @param obj The object to save
+           * @param auditUser The optional user identifier to use for the "By" audit fields
+           */
           async upsert (obj, auditUser) {
             return doSave(obj, auditUser, true)
           },
           /**
-                     * Delete an object by it's id
-                     * @param id The id of the object to delete
-                     * @returns {Promise<void>}
-                     */
+           * Delete an object by it's id
+           * @param id The id of the object to delete
+           * @returns {Promise<void>}
+           */
           async deleteById (id) {
             id = ensurePrefix(id)
             const multi = client.multi()
@@ -200,11 +215,11 @@ module.exports = {
             await multi.exec()
           },
           /**
-                     * Find all the objects stored in this collection, one page at a time
-                     * @param {number} page The page number to get
-                     * @param {number} size The number of objects to retrieve at a time
-                     * @param {boolean} reverse Whether to return records in reverse order
-                     */
+           * Find all the objects stored in this collection, one page at a time
+           * @param {number} page The page number to get
+           * @param {number} size The number of objects to retrieve at a time
+           * @param {boolean} reverse Whether to return records in reverse order
+           */
           async findAll (page = 0, size = 10, reverse = false) {
             page = parseInt(page)
             size = parseInt(size)
